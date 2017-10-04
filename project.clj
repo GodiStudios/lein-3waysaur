f1b7d31126973b5107ad8c3d361aa7ee469afc7b
Icaro Motta <imotta@thoughtworks.com>
2017-10-04 19:32:06 -0300

    Add basic lein plugin files

---
+++ project.clj
@@ -0,0 +1,6 @@
+(def project lein-3waysaur "0.1.0-SNAPSHOT"
+  :description "FIXME"
+  :url "https://github.com/GodiStudios/lein-3waysaur"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :eval-in-leiningen true)
---
+++ src/leiningen/lein_3waysaur.clj
@@ -0,0 +1,4 @@
+(ns leiningen.lein-3waysaur)
+
+(defn lein-3waysaur [project args]
+  (println "Hi!"))
