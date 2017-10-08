08e22056834a10f0651745d0fa6076e83fd69997
Julia Naomi <jnaomi@thoughtworks.com>
2017-10-07 21:47:00 -0300

    Corrected 'def project'to 'defproject' and added :description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(def project lein-3waysaur "0.1.0-SNAPSHOT"
-  :description "FIXME"
+(defproject lein-3waysaur "0.1.0-SNAPSHOT"
+  :description "Lein plugin for secure and developer friendly API"
   :url "https://github.com/GodiStudios/lein-3waysaur"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
